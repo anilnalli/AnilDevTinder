adbaefab0c19e0554009f1e97ea0ea764a69a92f
anilnalli <125810713+anilnalli@users.noreply.github.com>
2025-07-26 22:45:39 +0530

    routs post get delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,20 @@ const express =require("express");
 
 const app=express();
 
-app.use("/anil",(req,res)=>{
-  res.send("hello wolrd"); 
-})
-app.use("/kumar",(req,res)=>{
-  res.send("second change");
-})
-app.use((req,res)=>{
-    res.send("hello wolrd this is the express js code");
-})
+app.get('/userabc',(req,res)=>{
+  res.send({name:"anil",age:25})
+});
+
+app.post("/user",(req,res)=>{
+  res.send("data posted")
+
+});
+
+app.delete("/user",(req,res)=>{
+  res.send("data Deleted")
+});
+
+
 
 app.listen(3000,()=>{
     console.log("server stated successfully on port number 3000 ...")
