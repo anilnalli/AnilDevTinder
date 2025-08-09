0bbdbae1a5e633e2d699e906174e0f434fa64ffa
anilnalli <125810713+anilnalli@users.noreply.github.com>
2025-08-09 22:46:50 +0530

    POST PATCH DELETE UPDATE API CALLING. UPDATE TO THE DATABASE

--- src/app.js
+++ src/app.js
@@ -1,41 +1,63 @@
 const express = require("express");
-const{connectDB}=require('./config/database');
+const { connectDB } = require("./config/database");
 const app = express();
 const User = require("./models/user");
-const{AuthAdmin,userAuth}=require("./middleware/auth");
+const { AuthAdmin, UserAuth } = require("./middleware/auth");
 
-connectDB().then(()=>{
-  console.log("DB Connection succssfully established..");
-  app.listen(3000,()=>{
-    console.log("server stated successfully on port number 3000 ...");
+connectDB()
+  .then(() => {
+    console.log("DB Connection succssfully established..");
+    app.listen(3000, () => {
+      console.log("server stated successfully on port number 3000 ...");
+    });
+  })
+  .catch((err) => {
+    console.error("connection not established !!!");
   });
-}).catch((err)=>{
-  console.error("connection not established !!!");
-});
+
 app.use(express.json());
-app.use("/signup",async(req,res)=>{
+
+app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
     const savedUser = await user.save();
     res.status(201).send(savedUser);
   } catch (error) {
     res.status(400).send("Error saving user: " + error.message);
-    }
-});
-
-app.use("/admin/user",AuthAdmin,async(req,res)=>{
-  const user=new User({
-    firstName: "Admin",
-    lastName: "Userone",
-    email: "admin@gmail.com"
-  });
-  try{
-    const savedUser=await user.save();
-    res.status(201).send(savedUser);
-  }catch(error){
-    res.status(400).send("Error saving user: " + error.message);
   }
-
-})
-
-
+});
+app.get("/user", UserAuth, async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send("Error Fetching users: " + error.message);
+  }
+});
+app.delete("/user", AuthAdmin, async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const deletedUser = await User.findOneAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).send("User not found");
+    } else {
+      res.status(200).send("User deleted successfully");
+    }
+  } catch (error) {
+    res.status(500).send("Error Fetching users: " + error.message);
+  }
+});
+app.patch("/user", AuthAdmin, async (req, res) => {
+  const userdId = req.body.userId;
+  const updateDataa = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userdId, updateDataa);
+    if (!updatedUser) {
+      return res.status(404).send("User not found");
+    } else {
+      return res.status(200).send("User updated successfully");
+    }
+  } catch (error) {
+    res.status(500).send("Error Fetching users: " + error.message);
+  }
+});
